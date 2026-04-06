d6b9dca9a14e435b962d1eb11a098b4c1b8eba77
Alice Lin <aliclin@forcontu.com>
2026-04-06 17:25:08 -0600

    Modify code to pass autograder

--- 4_1a.php
+++ 4_1a.php
@@ -2,26 +2,21 @@
 
   $num1 = 41;
   $num2 = 2.1;
-  $intNum2 = (int)$num2;
 
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
+<html lang="en">
   <head>
-    <title>Activity 4.1a - Introduction to PHP</title>
     <meta charset="utf-8">
   </head>
 
   <body>
-    <h3>Activity 4.1a - Introduction to PHP</h3>
-     
     <?php 
       echo "The result of adding " . $num1 . " and " . $num2 . " is " . ($num1 + $num2) . ".<br>";
       echo "The result of subtracting " . $num1 . " and " . $num2 . " is " . ($num1 - $num2) . ".<br>";
       echo "The result of multiplying " . $num1 . " and " . $num2 . " is " . ($num1 * $num2) . ".<br>";
       echo "The result of dividing " . $num1 . " and " . $num2 . " is " . ($num1 / $num2) . ".<br>";
-      echo "The result of the remainder of dividing " . $num1 . " and " . $intNum2 . " is " . ($num1 % $intNum2) . ".";
+      echo "The result of the remainder of dividing " . $num1 . " and " . $num2 . " is " . ($num1 % $num2) . ".<br>";
     ?>
     
   </body>
