5f3fe2b98466dac32d578bab1386cf46392ea6c8
Alice Lin <aliclin@forcontu.com>
2026-04-28 11:02:59 -0600

    Complete 6.1b

--- 6_1a.php
+++ 6_1a.php
@@ -10,5 +10,30 @@ class Vehicle {
   protected $fuel_level = 0;
   protected $current_speed = 0;
   protected $state = "off";
-}
+  
+  public function getBrand() {
+    return $this->brand;
+  }
+
+  public function getSeats() {
+    return $this->seats;
+  }
+
+  public function getLicensePlate() {
+    return $this->license_plate;
+  }
+
+  public function setFuelLevel($fuel_level) {
+    $this->fuel_level = $fuel_level;
+  }
+
+  public function setCurrentSpeed($current_speed) {
+    $this->current_speed = $current_speed;
+  }
+
+  public function setState($state) {
+    $this->state = $state;
+  }
+  
+  }
 ?>
